bad7d617b93b335c2fc385928c5afcba1acec781
alexander <asvasi25@gmail.com>
2021-01-19 01:15:38 +0300

    changed shortest-word

--- codewars/arrays/shortest-word.js
+++ codewars/arrays/shortest-word.js
@@ -1,3 +1,10 @@
 // Simple, given a string of words, return the length of the shortest word(s).
 
 // String will never be empty and you do not need to account for different data types.
+
+function findShort(s){
+  return s
+    .split(' ')
+    .map(word => word.length)
+    .reduce((a, b) => Math.min(a, b));
+}
