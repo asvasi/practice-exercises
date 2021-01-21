89cb0feeaf213c7cd0c5349080ff8849cb993a09
alexander <asvasi25@gmail.com>
2021-01-22 00:27:59 +0300

    added reversed-string

--- codewars/strings-and-numbers/credit-card-mask.js
+++ codewars/strings-and-numbers/credit-card-mask.js
@@ -3,6 +3,7 @@
 // Your task is to write a function maskify, which changes all but the last four characters into '#'.
 
 // return masked string
+
 function maskify(cc) {
   return ('#'.repeat(Math.max(0, cc.length - 4)) + cc.slice(-4));
 }
