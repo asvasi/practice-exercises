312ff49bd10ad1eeb7c077580b122791e446a2c4
alexander <asvasi25@gmail.com>
2021-01-22 23:47:04 +0300

    fixed

--- codewars/strings-and-numbers/abbreviate-a-two-word-name.js
+++ codewars/strings-and-numbers/abbreviate-a-two-word-name.js
@@ -15,7 +15,7 @@ const abbrevName = name =>
     .join('.');
 
 
-function abbrevName(name){
+function abbrevName(name) {
   let nameArray = name.split(" ");
   return (nameArray[0][0] + "." + nameArray[1][0]).toUpperCase();
 }
